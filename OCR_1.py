c622b46552785a4cdb8403d28bde82d94265a274
eduardsosh01~ <eduardsosh@gmail.com>
2022-09-08 13:58:58 +0300

    trying1

--- OCR_1.py
+++ OCR_1.py
@@ -1,1 +1,14 @@
-print("starting")
+
+
+import pytesseract
+import PIL.Image
+import cv2
+
+
+
+
+myconfig = r"--psm 3 --oem 3"
+
+text = pytesseract.image_to_string(PIL.Image.open("dsc_2049_60809-564c6f63611b8.jpg"), config=myconfig)
+print(text)
+#print("starting")
