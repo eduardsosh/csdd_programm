de5d5a9eb4835a5da6f4a139d1dd40df58e3ace4
eduardsosh01~ <eduardsosh@gmail.com>
2022-09-08 16:10:52 +0300

    testing different configs

--- OCR_1.py
+++ OCR_1.py
@@ -8,10 +8,18 @@ import cv2
 
 
 
-#myconfig = r"--psm 3 --oem 3"
+myconfig1 = r"--psm 4 --oem 3" #psm 4 jo tas megina atrast dazada izmera tekstu oem 3 jo default
+myconfig2 = r"--psm 11 --oem 3"
+myconfig3 = r"--psm 13 --oem 3"
 
-text = pytesseract.image_to_string(PIL.Image.open("textimg.png"))
-print(text)
+text1 = pytesseract.image_to_string(PIL.Image.open("plate3.jpg"), config=myconfig1)
+text2 = pytesseract.image_to_string(PIL.Image.open("plate3.jpg"), config=myconfig2)
+text3 = pytesseract.image_to_string(PIL.Image.open("plate3.jpg"), config=myconfig3)
+print("Config1 rez: ")
+print(text1)
+print("Config2 rez: ")
+print(text2)
+print("Config3 rez: ")
+print(text3)
 
 
-#print("starting")
