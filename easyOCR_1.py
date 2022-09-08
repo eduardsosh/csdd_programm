3f5335eb28b0a360554bce77eb4414d30e14f015
eduardsosh01~ <eduardsosh@gmail.com>
2022-09-08 21:18:36 +0300

    ar komentiem

--- easyOCR_1.py
+++ easyOCR_1.py
@@ -3,26 +3,28 @@ import cv2
 #from matplotlib import pyplot as plt
 import numpy as np
 
-import warnings
+import warnings #atbrivojas no warningiem
+
 warnings.filterwarnings("ignore", category=UserWarning) 
 
 
-def has_numbers(inputString):
+def has_numbers(inputString): #parbauda vai string ir tikai skaitli.
     return any(char.isdigit() for char in inputString)
 
 
-image_path = 'carimg5.jpg'
+image_path = 'carimg5.jpg' #!!!!!!!!!sheit ir bildes path
+
 reader = easyocr.Reader(['en'], gpu=False)
 result = reader.readtext(image_path, detail=0)
 print(result)
 
-elon_array = np.array(result)
+elon_array = np.array(result) #List to array lai var stradat ar array nevis list
 
-print(elon_array)
+print(elon_array) #lai parbauditu
 
-for i in elon_array:
+for i in elon_array: # ejam cauri un meklejam to ko vajag
     if len(i) <8 and len(i) > 1 and "-" in i or ":" in i or " " in i:
-        print(i)
+        print(i) #izvadam atrasto pec filtriem
     
     
     
